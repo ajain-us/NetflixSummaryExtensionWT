4f6726a60b94efb8be560814f78727d6f13cb981
Aditya <adityajain_us@icloud.com>
2025-04-12 14:48:56 -0400

    all AI functions should work

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -4,7 +4,7 @@ output.innerHTML = "working!"
 
 async function callGemini(promptText) {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-001:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -27,13 +27,13 @@ netflix.addEventListener("click", () => {
             }else{
                 if(response.episode && response.progress){
                     output.innerHTML = "Thinking!"
-                    let result = await callGemini(`summarize ${response.title} up until episode ${response.episode}, keep it to 50 words`);
+                    let result = await callGemini(`summarize ${response.title} up until episode ${response.episode}, doing a sentence per episode.`);
                     output.innerHTML = result;
                 }else if(response.episode){
-                    output.innerHTML = `Title: ${response.title}, Episode: ${response.episode}`;
+                    let result = await callGemini(`summarize ${response.title} up until episode ${response.episode}, keep it to 100 words`);
+                    output.innerHTML = result; 
                 }else if(response.progress){
-                    let result = await callGemini(`summarize ${response.title} up until ${response.progress}, keep it to 50 words`);
-                    //output.innerHTML = `Title: ${response.title}, Progress: ${response.progress}`;
+                    let result = await callGemini(`summarize ${response.title} up until ${response.progress}, keep it to 100 words`);
                     output.innerHTML = result;
                 }else{
                     output.innerHTML = `Title: ${response.title}`;
