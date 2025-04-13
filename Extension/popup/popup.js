3c5a85fbe3cab9497af9193447603a1279293aeb
Matthew Maher <mmaher0722@gmail.com>
2025-04-13 02:38:47 -0400

    Added border plus misc
    Cleaned up code. Changed some colors and space between letters.

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -32,7 +32,7 @@ netflix.addEventListener("click", () => {
                     let result = await callGemini(`summarize what is going on in ${response.title} up until episode ${response.episode}, keep it to a short length and do not discuss events after the episode, no markdown`);
                     output.innerHTML = result;
                     output.removeAttribute("hidden");
-                } else if(response.episode){
+                } else if (response.episode) {
                     statusHeader.innerHTML = `Currently Summarizing: ${response.title}\n Current Episode: ${response.episode}`;
                     let result = await callGemini(`summarize what is going on in ${response.title} up until episode ${response.episode} keep it to a short length and do not discuss events after the episode, no markdown`);
                     output.removeAttribute("hidden")
@@ -42,7 +42,7 @@ netflix.addEventListener("click", () => {
                     let result = await callGemini(`summarize what is going on in ${response.title} up till ${response.progress} keep it to a medium length and do not discuss events after the point, no markdown`);
                     output.removeAttribute("hidden")
                     output.innerHTML = result;
-                } else if(response.title) {
+                } else if (response.title) {
                     statusHeader.innerHTML = `Current Summarizing: ${response.title}\n No Current Episode or Progress!`;
                     let result = await callGemini(`Summarize what is going on in ${response.title} keep it to a medium length and do not discuss events after the episode, no markdown`);
                     output.removeAttribute("hidden")
@@ -56,21 +56,18 @@ netflix.addEventListener("click", () => {
 });
 
 reviewButton.addEventListener("click", () => {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, {action: "netflix"}, (async response => {
             if (chrome.runtime.lastError) {
                 statusHeader.innerHTML = "This is not netflix!";
-            }else{
-                if(response.title){
+            } else {
+                if (response.title) {
                     statusHeader.innerHTML = `Currently Reviewing: ${response.title}`;
                     let result = await callGemini(`give a review of ${response.title}, keep it to 100 words, go easy on children shows, give it a star rating at the end out of 5 and no markdown please`);
                     output.removeAttribute("hidden");
                     output.innerHTML = "\t" + result;
-                }else{
-
                 }
             }
-            
         }));
     });
 });
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This project is an extension for Chrome. It works when you are on netflix in Goo
 You can use this extension by adding a file called "apikey.js" in the popup folder. This file should contain the following: "const GEMINI_API_KEY = 'YOUR_KEY';". Please replace 'YOUR_KEY' with a Google Gemini API key. This can be gotten for free from: https://ai.google.dev/gemini-api/docs/api-key. From there you can simply add the extenions folder to Google Extensions. 
 
 ## Usage
-To use this extension, select a TV show or movie that you would like information about. Then simply select the extension from the Chome extension selector. It will open a popup window that has two buttons, Summary and Review. From there, your request gets generated and displayed.
+To use this extension, select a TV show or movie within Netflix that you would like information about. Then simply select the extension from the Chome extension selector. It will open a popup window that has two buttons, Summary and Review. From there, your request gets generated and displayed.
